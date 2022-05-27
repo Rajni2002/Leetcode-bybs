052c059b54892aa6082901cd77b9044e85d179ae
Rajni2002 <rajnikantdash12@gmail.com>
2022-05-27 20:48:06 +0530

    Generate parenthesis added

--- medium.cpp
+++ medium.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
-#include<unordered_map>
-#include<vector>
+#include <unordered_map>
+#include <vector>
 using namespace std;
+
+void helper(int open, int close, vector<string> &v, string str)
+{
+    if (open == 0 && close == 0)
+    {
+        v.push_back(str);
+        return;
+    }
+    if (open == close)
+    {
+        helper(open - 1, close, v, str + "(");
+    }
+    if (open < close)
+    {
+        if (open == 0)
+        {
+            helper(open, close - 1, v, str + ")");
+        }
+        else
+        {
+            helper(open - 1, close, v, str + "(");
+            helper(open, close - 1, v, str + ")");
+        }
+    }
+}
+
+vector<string> generateParenthesis(int n)
+{
+    int open = n, close = n;
+    vector<string> v;
+    helper(open, close, v, "");
+    return v;
+}
 
 int totalFruit(vector<int> &fruits)
 {
